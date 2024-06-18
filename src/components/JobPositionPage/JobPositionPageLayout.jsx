572dc9c0f3d3b3eb94c71626fb7846ec504b75e5
박원빈 <harry21@kakao.com>
2024-06-18 19:13:14 +0900

    (add) JobPositionSummary Component

--- src/components/JobPositionPage/JobPositionPageLayout.jsx
+++ src/components/JobPositionPage/JobPositionPageLayout.jsx
@@ -1,3 +1,27 @@
+import styled from 'styled-components';
+import JobPositionSummary from './JobPositionSummary';
+
 export default function JobPositionPageLayout() {
-  return <div>JobPositionPageLayout</div>;
+  return (
+    <Wrap>
+      <JobPositionSummary />
+    </Wrap>
+  );
 }
+
+const Wrap = styled.main`
+  width: 100%;
+  max-width: 600px;
+  min-height: calc(100vh - 50px);
+
+  margin: 25px auto;
+  padding: 0px 40px;
+  border-radius: 30px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: white;
+  filter: drop-shadow(0px 4px 40px rgba(0, 0, 0, 0.12));
+`;
