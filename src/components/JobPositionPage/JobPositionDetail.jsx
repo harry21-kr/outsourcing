719b795ef68195eb1d306dce160b7c47373f03bf
박원빈 <harry21@kakao.com>
2024-06-19 21:38:38 +0900

    (add) JobPositionDetail, route

---
+++ src/components/JobPositionPage/JobPositionDetail.jsx
@@ -0,0 +1,101 @@
+import { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { JOB_POSITION } from '../../constants';
+import VideoDisplay from './VideoDisplay';
+
+export default function JobPositionDetail() {
+  const params = useParams();
+
+  const { title, highlight, summary, detail, imgUrl, videoId } = useMemo(
+    () => JOB_POSITION.filter((v) => v.position === params.position)[0],
+    [params.position]
+  );
+
+  return (
+    <Wrap>
+      <TitleText>{title}</TitleText>
+      <HighlightText>{highlight}</HighlightText>
+      <PositionImage src={imgUrl} width={412} height={410} />
+      <SummaryText>{summary}</SummaryText>
+      <DetailText>{detail}</DetailText>
+      <PositionVideoText>영상으로 보는 직업</PositionVideoText>
+      <PositionVideoWrapper>
+        <VideoDisplay videoId={videoId} width="100%" height="285px" />
+      </PositionVideoWrapper>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.main`
+  width: 100%;
+  min-width: 375px;
+  max-width: 600px;
+  min-height: calc(100vh - 50px);
+
+  margin: 25px auto;
+  padding: 40px;
+  border-radius: 30px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background: white;
+  filter: drop-shadow(0px 4px 40px rgba(0, 0, 0, 0.12));
+`;
+
+const TitleText = styled.h1`
+  font-size: 62px;
+  font-weight: bold;
+
+  margin-bottom: 28px;
+`;
+
+const HighlightText = styled.h2`
+  font-size: 24px;
+  color: #4b5563;
+
+  margin-bottom: 70px;
+`;
+
+const SummaryText = styled.h3`
+  width: 100%;
+  font-size: 28px;
+  font-weight: bold;
+  text-align: start;
+
+  margin-bottom: 14px;
+`;
+
+const DetailText = styled.p`
+  width: 100%;
+  font-size: 20px;
+  color: #4b5563;
+
+  margin-bottom: 70px;
+`;
+
+const PositionVideoText = styled.p`
+  width: 100%;
+  font-weight: bold;
+  font-size: 28px;
+
+  margin-bottom: 16px;
+`;
+
+const PositionVideoWrapper = styled.div`
+  width: 100%;
+  height: 285px;
+
+  border-radius: 20px;
+  background: black;
+
+  overflow: hidden;
+`;
+
+const PositionImage = styled.img`
+  background: black;
+
+  margin-bottom: 80px;
+`;
