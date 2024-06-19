ff3753ee590bb2a0a911d72e1b6d36902167de4c
LeeHareum <g029843507@gmail.com>
2024-06-20 01:43:59 +0900

    add:카카오톡공유,design:메인,버튼

---
+++ src/components/ResultPage/SharePage.style.jsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: 'Arial, sans-serif';
+`;
+
+export const Content = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 20px;
+`;
+
+export const StyledToastContainer = styled(ToastContainer)`
+  .Toastify__progress-bar--success {
+    background-color: #275cd7 !important;
+  }
+  .Toastify__progress-bar--error {
+    background-color: #d32f2f !important;
+  }
+  .toastify-icon {
+    color: #257dcf;
+  }
+`;
+
+export const SuccessIcon = styled(FaCheckCircle)`
+  color: #2f71d4;
+`;
+
+export const ErrorIcon = styled(FaTimesCircle)`
+  color: #f44336;
+`;
