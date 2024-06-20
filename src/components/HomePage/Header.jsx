3f56ee875d25d340f915a2b331280b13bec60c69
Dayeon-Cheon <dayeon.cheonn@gmail.com>
2024-06-20 14:08:58 +0900

    (delete) unnecessary logic

--- src/components/HomePage/Header.jsx
+++ src/components/HomePage/Header.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
 import SideBar from './SideBar';
 import styled from 'styled-components';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const navigate = useNavigate();
 
   const toggleSide = () => {
     setIsOpen(!isOpen);
   };
 
-  const goBack = () => {
-    navigate(-1);
-  };
-
-  const renderLeftContent = () => {
-    if (location.pathname === '/survey') {
-      return <FontAwesomeIcon onClick={goBack} icon={faAngleLeft} />;
-    }
-    return <Logo src="" alt="로고 이미지" />;
-  };
-
   return (
     <HeaderContainer>
-      <div>{renderLeftContent()}</div>
+      <div>
+        <Logo src="" alt="로고 이미지" />
+      </div>
       <div>
         <FontAwesomeIcon icon={faBars} onClick={toggleSide} />
         <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
