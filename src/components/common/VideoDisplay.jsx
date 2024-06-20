202262642064d297c67419e09686ec57fe5ee377
leeboa2005 <leeboa2005@naver.com>
2024-06-21 02:58:19 +0900

    update youtobe skeleton

--- src/components/common/VideoDisplay.jsx
+++ src/components/common/VideoDisplay.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import YouTube from 'react-youtube';
 import styled from 'styled-components';
 import SkeletonBox from './ui/Box';
+
 const VideoContainer = styled.div`
   position: relative;
   width: ${(props) => props.width || '520px'};
@@ -9,13 +10,11 @@ const VideoContainer = styled.div`
 `;
 
 function VideoDisplay({ videoId, width = '520px', height = '285px' }) {
-  const [isLoading, setIsLoading] = useState(true); // 비디오 로딩 상태 관리
+  const [isLoading, setIsLoading] = useState(true);
 
   return (
     <VideoContainer width={width} height={height}>
-      {' '}
-      {/* 비디오와 Skeleton을 동일한 위치에 배치하기 위한 컨테이너 */}
-      {isLoading && <SkeletonBox />} {/* 비디오 로딩 중일 때 SkeletonBox 표시 */}
+      {isLoading && <SkeletonBox />}
       <YouTube
         videoId={videoId}
         opts={{
@@ -23,10 +22,10 @@ function VideoDisplay({ videoId, width = '520px', height = '285px' }) {
             rel: 0,
             modestbranding: 1
           },
-          width: '100%', // 컨테이너 너비에 맞추기
-          height: '100%' // 컨테이너 높이에 맞추기
+          width,
+          height
         }}
-        onReady={() => setIsLoading(false)} // 비디오 로딩 완료 시 isLoading을 false로 설정
+        onReady={() => setIsLoading(false)}
         onEnd={(e) => {
           e.target.stopVideo(0);
         }}
--- src/components/common/ui/Box.js
+++ src/components/common/ui/Box.js
@@ -1,6 +1,5 @@
 import styled, { keyframes } from 'styled-components';
 
-// Define the shimmer keyframes animation
 const shimmer = keyframes`
   0% {
     background-position: -520px 0;
@@ -10,20 +9,18 @@ const shimmer = keyframes`
   }
 `;
 
-// Create the SkeletonBox component
 const SkeletonBox = styled.div`
-  width: 100%; // 부모 요소의 전체 너비 사용
-  height: 100%; // 부모 요소의 전체 높이 사용
+  width: 100%;
+  height: 100%;
   background: #f0f0f0;
   background-image: linear-gradient(to right, #f0f0f0 0%, #e0e0e0 20%, #f0f0f0 40%, #f0f0f0 100%);
   background-repeat: no-repeat;
   background-size: 1040px 100%;
-  position: absolute; // 부모 요소 내에서 절대 위치
+  position: absolute;
   top: 0;
   left: 0;
   overflow: hidden;
   animation: ${shimmer} 3s infinite linear;
-  border-radius: 10px;
 
   @media only screen and (max-width: 734px) {
     width: 90%;
