4c45601762aa5c0e9ade8cee3a2b1d1905e0f1a0
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-06-20 19:20:23 +0900

    Import 에러 이슈 관련 PR입니다.

--- src/components/HomePage/HomePageLayout.jsx
+++ src/components/HomePage/HomePageLayout.jsx
@@ -1,9 +1,8 @@
-import { Container, CenterTitle, Content, ImageContainer, MainImage, SubTitle } from './HomePageLayout.style';
-import mainImage from '../../../src/asset/images/programming.png';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '../../asset/button/Button';
 import styled from 'styled-components';
 import programming from '../../../src/asset/images/programming.png';
+import { Button } from '../../asset/button/Button';
+import { Content, ImageContainer, MainImage, TextContainer } from './HomePageLayout.style';
 
 export default function HomePageLayout() {
   const navigate = useNavigate();
@@ -14,15 +13,19 @@ export default function HomePageLayout() {
     <Wrap>
       <Content>
         <ImageContainer>
-          <MainImage style={{height:"300px", width:"300px"}} src={programming} alt="Main이미지" />
+          <MainImage style={{ height: '300px', width: '300px' }} src={programming} alt="Main이미지" />
         </ImageContainer>
         <TextContainer>
           <T26>어느날 갑자기 IT 회사에 들어간 나</T26>
-          <T48b>어떤 IT 직업을 <br/>
-            갖게 되었을까요?</T48b>
+          <T48b>
+            어떤 IT 직업을 <br />
+            갖게 되었을까요?
+          </T48b>
           <p>성격별 IT직종 적합테스트</p>
         </TextContainer>
-        <Button  variant="fill" onClick={handleTestClick}>입사 테스트하기</Button>
+        <Button variant="fill" onClick={handleTestClick}>
+          입사 테스트하기
+        </Button>
       </Content>
     </Wrap>
   );
@@ -50,10 +53,10 @@ const T48b = styled.span`
   color: #111827;
   margin-bottom: 40px;
   line-height: 110%;
-  letter-spacing : -2px;
+  letter-spacing: -2px;
 `;
 const T26 = styled.span`
- display: block;
+  display: block;
   font-family: 'Pretendard';
   font-size: 24px;
   font-weight: 500;
