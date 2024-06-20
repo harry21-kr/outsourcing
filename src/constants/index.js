786fb76f950c7c89df2ab68dbf20fa66529ec071
박원빈 <harry21@kakao.com>
2024-06-20 23:33:33 +0900

    (fix) vercel image path error

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,7 +6,7 @@ export const JOB_POSITION = [
     detail:
       '프론트엔드 개발자는 웹 애플리케이션의 인터페이스를 구현하고 사용자 경험을 최적화하는 역할을 합니다. HTML, CSS, JavaScript를 사용하여 인터랙티브하고 반응형 웹 페이지를 만들어내며, 디자인과 기능성을 조화롭게 통합하는 능력이 요구됩니다.',
     label: '개발',
-    imgUrl: '/src/asset/images/programming.png',
+    imgUrl: '/public/images/programming.png',
     videoId: 'ceJMyCfYuwc'
   },
   {
@@ -16,7 +16,7 @@ export const JOB_POSITION = [
     detail:
       '백엔드 개발자는 서버, 데이터베이스, 애플리케이션 로직을 관리하며 웹 애플리케이션의 핵심 기능을 구현합니다. 데이터 처리, API 개발, 서버 관리 등을 통해 사용자에게 신뢰성 있고 빠른 서비스를 제공하는 것이 주된 역할입니다.',
     label: '개발',
-    imgUrl: '/src/asset/images/back_end.png',
+    imgUrl: '/public/images/back_end.png',
     videoId: '4pv6vJgr4gU'
   },
   {
@@ -26,7 +26,7 @@ export const JOB_POSITION = [
     detail:
       'UI/UX 디자이너는 사용자의 요구와 기대를 반영하여 인터페이스를 설계하고 최적의 사용자 경험을 제공합니다. 사용자 조사와 테스트를 통해 인사이트를 도출하고, 이를 바탕으로 직관적이고 아름다운 인터페이스를 디자인합니다. 사용자 흐름, 정보 구조, 시각적 요소를 통합하여 제품이 사용자에게 즐겁고 효율적인 경험을 제공하도록 하는 것이 주된 역할입니다.',
     label: '디자인',
-    imgUrl: '/src/asset/images/ui_ux_designer.png',
+    imgUrl: '/public/images/ui_ux_designer.png',
     videoId: 'pGB1trC8x5c'
   },
   {
@@ -36,7 +36,7 @@ export const JOB_POSITION = [
     detail:
       '프로덕트 디자이너는 제품의 전체적인 사용자 경험을 설계하고, 사용자 요구에 부합하는 기능적이고 아름다운 제품을 만들어냅니다. 사용자 조사, 프로토타이핑, 사용자 테스트를 통해 제품의 모든 측면을 최적화하는 것이 주된 역할입니다.',
     label: '디자인',
-    imgUrl: '/src/asset/images/product_designer.png',
+    imgUrl: '/public/images/product_designer.png',
     videoId: '9WmGat-8eNs'
   },
   {
@@ -46,7 +46,7 @@ export const JOB_POSITION = [
     detail:
       '서비스 기획자는 사용자와 시장의 요구를 분석하여 최적의 서비스 전략을 수립하고 구현합니다. 시장 조사, 데이터 분석, 서비스 디자인 등을 통해 사용자의 요구를 충족시키는 서비스 경험을 제공하는 것이 주된 역할입니다.',
     label: '기획',
-    imgUrl: '/src/asset/images/service_planner.png',
+    imgUrl: '/public/images/service_planner.png',
     videoId: '_LuAV2-bqIo'
   },
   {
@@ -56,7 +56,7 @@ export const JOB_POSITION = [
     detail:
       '프로덕트 매니저는 제품 개발의 모든 단계를 관리하고 조정하여 성공적인 제품을 출시합니다. 제품 전략 수립, 프로젝트 관리, 팀 협업 등을 통해 제품의 비전과 목표를 달성하는 것이 주된 역할입니다.',
     label: '기획',
-    imgUrl: '/src/asset/images/product_manager.png',
+    imgUrl: '/public/images/product_manager.png',
     videoId: '1XlV1AJbXI8'
   }
 ];
